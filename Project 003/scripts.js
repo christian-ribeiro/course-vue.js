1bfd818e6c4d16150f7a8987b20aea093446f35c
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2024-10-10 21:38:26 -0300

    use local storage

--- Project 003/scripts.js
+++ Project 003/scripts.js
@@ -17,6 +17,12 @@ const handlingForms = {
             this.tasks.push(this.newTask)
             this.newTask = {}
         }
+    },
+    created() {
+        this.tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : this.tasks;
+    },
+    updated() {
+        localStorage.setItem("tasks", JSON.stringify(this.tasks));
     }
 }
 
