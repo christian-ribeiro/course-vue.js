5f8840ae75c0cc78b897f22e34932351f4971715
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2024-10-13 06:04:12 -0300

    using props and computed in composite api

--- Project-006/src/App.vue
+++ Project-006/src/App.vue
@@ -1,37 +1,31 @@
 <script setup>
-import { ref, reactive } from "vue";
-
-let count = ref(0);
+import { reactive } from "vue";
+import Progress from "./components/Progress.vue";
 
 let courses = reactive([
   {
     title: "Javascript",
+    done: true,
   },
   {
     title: "React",
+    done: false,
   },
   {
     title: "Vue",
+    done: true,
   },
 ]);
 
-let newCourse = {};
-
-function increment() {
-  count.value++;
-}
+let newCourse = { done: false };
 
 function addCourse() {
   courses.push(newCourse);
-  newCourse = {};
+  newCourse = { done: false };
 }
 </script>
 
 <template>
-  <div>
-    <h1>{{ count }}</h1>
-    <button @click="increment">Increment</button>
-  </div>
   <div>
     <h1>Courses list</h1>
     <ul>
@@ -42,6 +36,7 @@ function addCourse() {
     <br />
     <input type="text" v-model="newCourse.title" />
     <button @click="addCourse" type="button">Add</button>
+    <Progress :courses="courses"></Progress>
   </div>
 </template>
 
---
+++ Project-006/src/components/Progress.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["courses"]);
+
+const coursesDone = computed(() => {
+  return props.courses.filter((item) => item.done).length;
+});
+</script>
+
+<template>
+  <div>
+    {{ `${coursesDone} cursos completos de um total de ${courses.length}` }}
+  </div>
+</template>
