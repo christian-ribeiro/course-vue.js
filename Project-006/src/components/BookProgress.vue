cc92b6f376ad6daf0e061fef4c46fb11f15ca1b8
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2024-10-13 07:24:29 -0300

    computed property for message

--- Project-006/src/components/BookProgress.vue
+++ Project-006/src/components/BookProgress.vue
@@ -6,12 +6,18 @@ const props = defineProps(["books"]);
 const booksRead = computed(() => {
   return props.books.filter((book) => book.isRead).length;
 });
+
+const booksReadMessage = computed(() => {
+  return booksRead.value >= props.books.length
+    ? "Okay, done"
+    : `${booksRead.value} of ${props.books.length} read`;
+});
 </script>
 
 <template>
   <div class="books-read">
     <label for="progress">Seu Progresso</label><br />
     <progress :value="booksRead" :max="books.length"></progress>
-    <p>{{ `${booksRead} de ${books.length} livros lidos` }}</p>
+    <p>{{ booksReadMessage }}</p>
   </div>
 </template>
