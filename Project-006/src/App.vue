8a91872629009fda1298499f8e6b47263d3594ff
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2024-10-13 05:55:07 -0300

    using reactive for objects

--- Project-006/src/App.vue
+++ Project-006/src/App.vue
@@ -1,10 +1,29 @@
 <script setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 let count = ref(0);
 
+let courses = reactive([
+  {
+    title: "Javascript",
+  },
+  {
+    title: "React",
+  },
+  {
+    title: "Vue",
+  },
+]);
+
+let newCourse = {};
+
 function increment() {
   count.value++;
+}
+
+function addCourse() {
+  courses.push(newCourse);
+  newCourse = {};
 }
 </script>
 
@@ -12,6 +31,17 @@ function increment() {
   <div>
     <h1>{{ count }}</h1>
     <button @click="increment">Increment</button>
+  </div>
+  <div>
+    <h1>Courses list</h1>
+    <ul>
+      <li v-for="course in courses" :key="course.title">
+        {{ course.title }}
+      </li>
+    </ul>
+    <br />
+    <input type="text" v-model="newCourse.title" />
+    <button @click="addCourse" type="button">Add</button>
   </div>
 </template>
 
