37dbb8db2bb75e6de66e343ca2d958ecd05c813f
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2024-10-13 05:46:14 -0300

    increment value using ref

--- Project-006/src/App.vue
+++ Project-006/src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
+import { ref } from "vue";
+
+let count = ref(0);
 </script>
 
 <template>
   <div>
-    <h1>Hello</h1>
+    <h1>{{ count }}</h1>
+    <button @click="count++">Increment</button>
   </div>
 </template>
 
